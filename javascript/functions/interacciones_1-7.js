5226fa179338cbfb4b5ea3c8b894b17c981f05a2
Alsamu92 <alexsanzm@email.com>
2023-09-28 17:58:40 +0200

    más funciones

--- javascript/functions/interacciones_1-7.js
+++ javascript/functions/interacciones_1-7.js
@@ -12,22 +12,17 @@ sum(3,6)
 
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
 
-
-const cadenaLarga=(unArray)=>{
-    let longitudMax =unArray[0]
-    for(let i=0;i<unArray.length;i++){
-    if(unArray[i].length>longitudMax.length){
-        longitudMax=unArray[i]
+const masLarga=(unArray)=>{
+  let maxLen=unArray[0];
+  for(let i =0;i<unArray.length;i++){
+    if(unArray[i].length>maxLen.length){
+      maxLen=unArray[i]
     }
   
+  }
+  return maxLen
 }
-return longitudMax
-
-
-}
-
-console.log(cadenaLarga(avengers))
-
+console.log(masLarga(avengers))
 
 
 
@@ -38,6 +33,45 @@ console.log(cadenaLarga(avengers))
 // Implemente la función denominada sumNumbers que toma un array de números
 //  como argumento y devuelve la suma de todos los números de la matriz. 
 
-// Puedes usar este array para probar tu función:
+const numbers = [1, 2, 3, 5, 45, 37, 58];
 
+const suma=(otroArray)=>{
+  let acumulado = 0
+  for(let a =0;a<otroArray.length;a++){
+    acumulado+=otroArray[a]
+  }
+  return acumulado
+}
+console.log (suma(numbers))
 
+// **Iteración #4: Calcular el promedio**
+
+// Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:
+const nuevosNumeros = [12, 21, 38, 5, 45, 37, 6];
+
+const promedio=(tuArray)=>{
+  let sumatotal = 0
+  for(let b =0;b<tuArray.length;b++){
+    sumatotal+=tuArray[b]
+  }
+  let media=sumatotal/tuArray.length
+  return media
+}
+console.log (promedio(nuevosNumeros))
+
+// **Iteración #5: Calcular promedio de strings** Crea una función que reciba por 
+// parámetro un array y cuando es un valor number lo sume y de lo contrario cuente la 
+// longitud del string y lo sume. Puedes usar este array para probar tu función:
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub']
+
+// const contar =(miArray)=>{
+//   let acumuladorString =0
+//   let acumuladorNumber
+//   for(let c=0;c<miArray.length;c++){
+//     typeof miArray[c]=="string" ? miArray[c].length+=acumuladorString:miArray[c]+=acumuladorNumber
+//   }
+  
+//   return acumulador
+// }
+console.log(typeof mixedElements[0])
+console.log(contar(mixedElements))
