59d77acd87165f007371ea751a2af99981e4f239
Alsamu92 <alexsanzm@email.com>
2023-09-30 23:42:40 +0200

    segundo de repaso

---
+++ javascript/Bucles/repetidos.js
@@ -0,0 +1,12 @@
+const materiales = [
+    "folio",
+    "carpeta",
+    "boli",
+    "lapiz",
+    "boli",
+    "carpeta",
+    "folio",
+    "lapiz",
+    "folio",
+  ];
+// ----_> creamos la variable que se encarga de guardar la solucion del problema
