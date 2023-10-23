cc5c1db4bfd68a9568f61203a03a4ea18d6eb424
Alsamu92 <alexsanzm@email.com>
2023-10-23 12:54:06 +0200

    quitar node module

---
+++ NODE/Express/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const PORT = 8080;
+const app = express();
+
+const router = express.Router();
+
+router.get("/lista", (req, res, next) => {
+  const lista = [
+    // . { nombre: "Manzanas", precio: 2.5 },
+      { nombre: "Plátanos", precio: 1.8 },
+      { nombre: "Leche", precio: 1.2 },
+      { nombre: "Huevos", precio: 3.0 },
+      { nombre: "Pan", precio: 2.0 },
+      { nombre: "Arroz", precio: 1.5 },
+      { nombre: "Pasta", precio: 1.0 },
+      { nombre: "Aceite de oliva", precio: 4.5 },
+      { nombre: "Pollo", precio: 5.0 },
+      { nombre: "Pescado", precio: 7.0 },
+      { nombre: "Carne molida", precio: 6.0 },
+      { nombre: "Cerveza", precio: 2.2 },
+      { nombre: "Vino tinto", precio: 8.0 },
+      { nombre: "Queso", precio: 3.5 },
+      { nombre: "Yogur", precio: 1.2 },
+      { nombre: "Cereal", precio: 4.0 },
+      { nombre: "Tomates", precio: 2.0 },
+      { nombre: "Cebollas", precio: 1.0 },
+      { nombre: "Pimientos", precio: 1.5 },
+      { nombre: "Zanahorias", precio: 1.0 },
+      { nombre: "Papas", precio: 1.2 },
+      { nombre: "Aguacate", precio: 2.0 },
+      { nombre: "Café", precio: 5.5 },
+      { nombre: "Té", precio: 3.0 },
+      { nombre: "Galletas", precio: 2.2 },
+      { nombre: "Chocolates", precio: 1.8 },
+      { nombre: "Sopa enlatada", precio: 2.5 },
+      { nombre: "Detergente", precio: 4.0 },
+      { nombre: "Papel higiénico", precio: 1.5 },
+      { nombre: "Toallas de papel", precio: 2.0 },
+      { nombre: "Jabón de baño", precio: 1.2 },
+      { nombre: "Shampoo", precio: 3.0 }
+  ];
+  res.send(lista);
+});
+
+app.use("/api/v1", router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
