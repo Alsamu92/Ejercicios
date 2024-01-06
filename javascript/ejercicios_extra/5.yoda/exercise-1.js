84ae5fda2b5328571ed411d7c577802740abcf64
Alex <alexsanzm@gmail.com>
2024-01-06 22:30:58 +0100

    ejercicios repaso 5

--- javascript/ejercicios_extra/5.yoda/exercise-1.js
+++ javascript/ejercicios_extra/5.yoda/exercise-1.js
@@ -1,18 +1,44 @@
 
 
+
+
 const allCharacters=async()=>{
-    const pjReducido=[]
+    const allPersonajes=[]
+  
     const data=await fetch("https://hp-api.onrender.com/api/characters")
     const dataJson=await data.json()
-    pjReducido.push(dataJson.slice(0,20))
-    
-mapData(pjReducido[0])
+    allPersonajes.push(dataJson.slice(0,20))
+
+ 
+mapData( allPersonajes[0])
 }
 const mapData=(array)=>{
-    array.map((personaje)=>{
-        console.log(personaje.name)
+   const dataMap= array.map((personaje)=>{
+     return {  name:personaje.name,
+        img:personaje.image}
         })
+        printCard(dataMap)
 }
+const printCard=(card)=>{
+
+card.forEach((c)=>{
+   printCardCharacter(c)
+})
+
+
+}
+
+const printCardCharacter = (datos) => {
+        const template = ({ name,img }) => `
+            <figure>
+                <h4>${name}</h4>
+                <img src=${img}></img>
+            </figure>
+        `;
+        const body = document.querySelector("body");
+        body.innerHTML += template(datos);
+    
+};
 allCharacters()
 
 
